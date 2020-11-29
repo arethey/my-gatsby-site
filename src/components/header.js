70c89921eb22a89e176beb3755fc66d571da2d7f
ardieD <ardie.derrayal@webriq.services>
2020-11-29 10:03:14 +0800

    banner

--- src/components/header.js
+++ src/components/header.js
@@ -3,56 +3,53 @@ import PropTypes from "prop-types"
 import React, { useEffect, useState } from "react"
 
 const Header = ({ siteTitle }) => {
-  const [scroll, setScroll] = useState(false)
+  // const [scroll, setScroll] = useState(false)
 
-  const handleScroll = () => setScroll(window.pageYOffset > 0)
+  // const handleScroll = () => setScroll(window.pageYOffset > 0)
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll)
+  // useEffect(() => {
+  //   window.addEventListener("scroll", handleScroll)
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll)
-    }
-  }, [handleScroll])
+  //   return () => {
+  //     window.removeEventListener("scroll", handleScroll)
+  //   }
+  // }, [handleScroll])
 
-  let styles = {
-    background: `rebeccapurple`,
-    marginBottom: `1.45rem`,
-    padding: `1.45rem 1.0875rem`,
-    transition: "all 0.3s",
-  }
-  if (scroll) {
-    styles = {
-      ...styles,
+  // let styles = {
+  //   background: `rebeccapurple`,
+  //   marginBottom: `1.45rem`,
+  //   padding: `1.45rem 1.0875rem`,
+  //   transition: "all 0.3s",
+  // }
+  // if (scroll) {
+  //   styles = {
+  //     ...styles,
+  //     position: "fixed",
+  //     top: 0,
+  //     width: "100%",
+  //     background: "#fff",
+  //     padding: `.45rem 1.0875rem`,
+  //     boxShadow: "0 8px 6px -6px #999",
+  //     zIndex: 999,
+  //   }
+  // }
+
+  const styles = {
+    nav: {
       position: "fixed",
       top: 0,
       width: "100%",
       background: "#fff",
-      padding: `.45rem 1.0875rem`,
+      padding: `1rem 1.5rem`,
       boxShadow: "0 8px 6px -6px #999",
       zIndex: 999,
-    }
+    },
   }
 
   return (
-    <header style={styles}>
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-        }}
-      >
-        <h1 style={{ margin: 0 }}>
-          <Link
-            to="/"
-            style={{
-              color: scroll ? "black" : "white",
-              textDecoration: `none`,
-            }}
-          >
-            {siteTitle}
-          </Link>
-        </h1>
+    <header>
+      <div style={styles.nav}>
+        <Link to="/">{siteTitle}</Link>
       </div>
     </header>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,46 +1,78 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
+const IndexPage = () => {
+  return (
+    <Layout>
+      <SEO title="Home" />
 
-    <form name="contact-form" method="post" data-netlify="true">
-      <input type="hidden" name="form-name" value="contact-form"></input>
-      <div>
-        <label htmlFor="name">Name</label>
-        <br />
-        <input
-          id="name"
-          name="name"
-          placeholder="Your name"
-          type="text"
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <br />
-        <input
-          id="email"
-          name="email"
-          placeholder="Your email"
-          type="email"
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="message">Message</label>
-        <br />
-        <textarea id="message" name="message" />
-      </div>
-      <button>Send</button>
-    </form>
-  </Layout>
-)
+      <section className="banner">
+        <div className="container">
+          <div className="row justify-content-center align-items-center">
+            <div className="col-md-8 text-white">
+              <h1>Find the perfect house for you & change your life!</h1>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book.
+              </p>
+            </div>
+            <div className="col-md-4">
+              <div className="bg-white pt-3">
+                <h4 className="text-center">Don't miss the opportunity</h4>
+                <form name="contact-form" method="post" data-netlify="true">
+                  <div className="p-3">
+                    <input
+                      type="hidden"
+                      name="form-name"
+                      value="contact-form"
+                    />
+                    <div class="form-group">
+                      <label htmlFor="name">Name</label>
+                      <input
+                        class="form-control"
+                        id="name"
+                        name="name"
+                        type="text"
+                        required
+                      />
+                    </div>
+                    <div class="form-group">
+                      <label htmlFor="email">Email</label>
+                      <input
+                        class="form-control"
+                        id="email"
+                        name="email"
+                        type="email"
+                        required
+                      />
+                    </div>
+                    <div class="form-group">
+                      <label htmlFor="number">Phone number</label>
+                      <input
+                        class="form-control"
+                        id="number"
+                        name="number"
+                        type="number"
+                        required
+                      />
+                    </div>
+                  </div>
+                  <div className="contact-action py-2">
+                    <button className="btn border-0 btn-block text-uppercase">
+                      submit now
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  )
+}
 
 export default IndexPage
